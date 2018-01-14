f94090770c4a0a8449afdbddc579858a4d1cfd01
ShwetabhK <shwetabh002@gmail.com>
2018-01-14 18:57:49 +0530

    Ecommerce-Django-Website-commit3

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -6,7 +6,7 @@ class ContactForm(forms.Form):
             attrs={"class":"form-control","placeholder":"Name" ,"id":"form_full"}))
     email=forms.EmailField(
         widget=forms.EmailInput(
-            attrs={"class":"form-control","placeholder":"Email" ,"id":"form_full"}))
+            attrs={"class":"form-control","placeholder":"Email" ,"id":"email"}))
     content=forms.CharField(
         widget=forms.Textarea(
             attrs={"class":"form-control","placeholder":"Type your content here","id":"form_content"}
@@ -16,3 +16,7 @@ class ContactForm(forms.Form):
         email =self.cleaned_data.get("email")
         if not "gmail.com" in email:
             raise forms.ValidationError("Email has to be gmail.com")
+        return email
+class LoginForm(forms.Form):
+    username=forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
+    password=forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
