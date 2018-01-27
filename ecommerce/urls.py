099a9af58b60ebc029bc0d6a284f7f3bcdef7000
ShwetabhK <shwetabh002@gmail.com>
2018-01-27 17:10:26 +0530

    Ecommerce-Django-Website-commit5

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from .view import home_page,about_page,contact_page,login_page,register_page
 urlpatterns = [
     url(r'^$',home_page),
@@ -24,3 +26,6 @@ urlpatterns = [
     url(r'^about/$',about_page),
     url(r'^admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
